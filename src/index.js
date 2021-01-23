82cb9c0054880e50b41fde3db9583dbc8e7b00c9
James Thomas <codeactor@protonmail.com>
2021-01-23 15:37:46 +0000

    Initial use of SASS

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import './renderContent.js';
 import './search.js';
 import './index.css';
+import './index.scss';
 import '../images/succ.png';
 import '../images/sudoku_solver.png';
 import '../images/self.site.png';
